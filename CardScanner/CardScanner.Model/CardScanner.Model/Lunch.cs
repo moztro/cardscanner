7f8bb45bc025edf32fa5ef1937957486bfdfafcb
FernandoCanul <fernando.canul@gmail.com>
2017-02-26 10:54:51 -0600

    modify config window

--- CardScanner/CardScanner.Model/CardScanner.Model/Lunch.cs
+++ CardScanner/CardScanner.Model/CardScanner.Model/Lunch.cs
@@ -24,4 +24,42 @@ namespace CardScanner.Model
             }
         }
     }
+
+    public class COMPort
+    {
+        public int Id { get; set; }
+
+        public string COM { get; set; }
+
+        public static ICollection<COMPort> COMPorts = new List<COMPort>();
+    }
+
+    public class BaudRate
+    {
+        public int Id { get; set; }
+
+        public int Value { get; set; }
+
+        public static IEnumerable<BaudRate> BaudRates
+        {
+            get
+            {
+                return new List<BaudRate>
+                {
+                    new BaudRate { Id = 1, Value = 300 },
+                    new BaudRate { Id = 2, Value = 1200 },
+                    new BaudRate { Id = 2, Value = 2400 },
+                    new BaudRate { Id = 2, Value = 4800 },
+                    new BaudRate { Id = 2, Value = 9600 },
+                    new BaudRate { Id = 2, Value = 14400 },
+                    new BaudRate { Id = 2, Value = 19200 },
+                    new BaudRate { Id = 2, Value = 28800 },
+                    new BaudRate { Id = 2, Value = 38400 },
+                    new BaudRate { Id = 2, Value = 57600 },
+                    new BaudRate { Id = 2, Value = 115200 },
+                    new BaudRate { Id = 2, Value = 230400 }  
+                };
+            }
+        }
+    }
 }
--- CardScanner/CardScanner.UI/SerialPortConfig.xaml.cs
+++ CardScanner/CardScanner.UI/SerialPortConfig.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using System.IO.Ports;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
+using CardScanner.Model;
 
 namespace CardScanner.UI
 {
@@ -22,56 +23,98 @@ namespace CardScanner.UI
     /// </summary>
     public partial class SerialPortConfig : MetroWindow
     {
-        SerialPort serialPort = new SerialPort();
+        internal SerialPort serialPort = new SerialPort();
+        private int _dataBits = 8;
+        private Handshake _handshake = Handshake.None;
+        private Parity _parity = Parity.None;        
+        private StopBits _stopBits = StopBits.One;
+
+        /// <summary> 
+        /// Holds data received until we get a terminator. 
+        /// </summary> 
+        private string tString = string.Empty;
+        /// <summary> 
+        /// End of transmition byte in this case EOT (ASCII 4). 
+        /// </summary> 
+        private byte terminator = 0x4;
+
         public SerialPortConfig()
         {
             InitializeComponent();            
-            InitializeSerialPorts();
-            //Init();
+            InitializeSerialPorts();            
         }
 
         private void InitializeSerialPorts()
         {
             var ArrayComPortsNames = SerialPort.GetPortNames();
-            
+            var i = 0;
             foreach(string array in ArrayComPortsNames)
             {
-                ports.Items.Add(array);
+                
+                Model.COMPort port = new COMPort();
+                port.Id = i;
+                port.COM = array;
+                Model.COMPort.COMPorts.Add(port);
+                i++;
             }
-
-            baudRate.Items.Add(300);
-            baudRate.Items.Add(600);
-            baudRate.Items.Add(1200);
-            baudRate.Items.Add(2400);
-            baudRate.Items.Add(9600);
-            baudRate.Items.Add(14400);
-            baudRate.Items.Add(19200);
-            baudRate.Items.Add(38400);
-            baudRate.Items.Add(57600);
-            baudRate.Items.Add(115200);
-        }
-
-        private async void Init()
-        {
-            var task = await this.ShowProgressAsync("Serial Ports Configuration", "Reading your serial ports!!!");
-
-            task.Maximum = 100;
-            for (int i = 0; i <= task.Maximum; i += 10)
-            {
-                task.SetProgress(i);
-                await Task.Delay(100);
-            }
-
-            if (task.IsOpen)
-            {
-                await task.CloseAsync();                
-            }
+            Ports.ItemsSource = Model.COMPort.COMPorts;
+            Ports.DisplayMemberPath = "COM";
+            BaudRates.ItemsSource = Model.BaudRate.BaudRates;
+            BaudRates.DisplayMemberPath = "Value";
         }
 
         private async void button_Click(object sender, RoutedEventArgs e)
         {
-            await this.ShowMessageAsync("Config Finish", "Config has been done, ready for scan ;)");
-            this.Close();
+            if (this.Open())
+            {
+                await this.ShowMessageAsync("Config Finish", "Config has been done, ready for scan ;)");
+                this.Close();
+            }else
+            {
+                await this.ShowMessageAsync("Config Error", "An error ocurred :/");
+            }
+
+        }
+
+        public bool Open()
+        {
+            try
+            {
+                serialPort.BaudRate = ((BaudRate) this.BaudRates.SelectedItem).Value;
+                serialPort.DataBits = _dataBits;
+                serialPort.Handshake = _handshake;
+                serialPort.Parity = _parity;
+                serialPort.PortName = ((COMPort) this.Ports.SelectedItem).COM;
+                serialPort.StopBits = _stopBits;
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataReceived);
+                serialPort.Open();
+            }
+            catch { return false; }
+            return true;
+        }
+
+        void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //Initialize a buffer to hold the received data 
+            byte[] buffer = new byte[serialPort.ReadBufferSize];
+
+            //There is no accurate method for checking how many bytes are read 
+            //unless you check the return from the Read method 
+            int bytesRead = serialPort.Read(buffer, 0, buffer.Length);
+
+            //For the example assume the data we are received is ASCII data. 
+            tString += Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            //Check if string contains the terminator  
+            if (tString.IndexOf((char)terminator) > -1)
+            {
+                //If tString does contain terminator we cannot assume that it is the last character received 
+                string workingString = tString.Substring(0, tString.IndexOf((char)terminator));
+                //Remove the data up to the terminator from tString 
+                tString = tString.Substring(tString.IndexOf((char)terminator));
+                //Do something with workingString 
+                Console.WriteLine(workingString);
+            }
+            else { Console.WriteLine(tString); }
         }
     }
 }
